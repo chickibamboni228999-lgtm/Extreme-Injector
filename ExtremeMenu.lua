82b4b5dadb33f9aed1681974c9629c79c45649d5
chickibamboni228999-lgtm <chickibamboni228999@gmail.com>
2025-09-04 09:02:00 +0300

    ExtremeMenu.lua

---
+++ ExtremeMenu.lua
@@ -0,0 +1,14 @@
+-- Подключаем модуль
+local extremeMenu = loadstring(game:HttpGet("https://raw.githubusercontent.com/chickibamboni228999-lgtm/Extreme-Injector/refs/heads/main/Module.lua"))()
+
+-- Включаем флай
+extremeMenu.fly()
+
+-- Включаем ESP
+extremeMenu.esp()
+
+-- Меняем скорость
+extremeMenu.setSpeed(50)
+
+-- Флинг
+extremeMenu.flingNearest()
